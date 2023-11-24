39129d448b71cf3989d760b69d8f6d63971e65d0
johnmisquitta <34851009+johnmisquitta@users.noreply.github.com>
2023-11-24 19:11:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import jax.numpy as jnp
+import soundfile as sf
 st.set_page_config(layout="wide")
 st.header("Pick an Audio File")
 uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "ogg"])
